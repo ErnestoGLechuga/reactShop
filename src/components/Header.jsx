94853d7aa7806ad2389ca0205670a19b0a4b8a06
Ernesto Lechuga <bicerya2023@gmail.com>
2023-04-09 16:51:47 -0600

    se anade el uso de hooks y se integra la API

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '@styles/Header.scss';
+import Menu from '@components/Menu'
 import shopCartIcon from '@icons/icon_shopping_cart.svg';
 import logoYard from '@logos/logo_yard_sale.svg';
 import iconMenu from '@icons/icon_menu.svg';
 
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+
     return (
-        <nav>
+    <nav>
         <img src={iconMenu} alt="menu" className="menu"/>
-    
         <div className="navbar-left">
             <img src={logoYard} alt="Logo" className="logoNormal"/>
             <ul>
@@ -34,13 +41,16 @@ const Header = () => {
         </div>
         <div className="navbar-right">
             <ul>
-                <li className="navbar-email">ernesto.lechuga@zuzu.com</li>
+                <li className="navbar-email" onClick={handleToggle}>
+                    ernesto.lechuga@zuzu.com
+                </li>
                 <li className="navbar-shopping-cart">
                     <img src={shopCartIcon} alt="shopping cart"/>
                     <div>2</div>
                 </li>
             </ul>
         </div>
+        {toggle && <Menu />}
     </nav>
     );
 }
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/ProductItem.scss';
-//const productPhotoUrl = require('https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940').default;
 import addToCart from '../../maquetacion/icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
+	const [cart, setCart] = useState([]);
+
+	const handleCartClick = () => {
+		setCart([]);
+	}
+
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
-				<figure>
+				<figure onClick={handleCartClick}>
 					<img src={addToCart} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
